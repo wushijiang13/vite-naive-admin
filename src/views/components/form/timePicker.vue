5f85484bdc5c4fec15836e03e779575c94f972da
mr.wu <wushijiang@huan.tv>
2023-04-20 18:49:14 +0800

    feat: 调整timePicker模块，固定时间点组件展示

--- src/views/components/form/timePicker.vue
+++ src/views/components/form/timePicker.vue
@@ -3,7 +3,11 @@
     <n-descriptions bordered label-placement="top" class="desc-box">
       <n-descriptions-item label="固定时间点">
         <n-space>
-          <n-date-picker v-model:value="time" type="date" placeholder="选择时间"/>
+          <n-select class="select-sl"  filterable  v-model:value="time" :options="option" >
+            <template #arrow>
+              <AccessTimeFilled/>
+            </template>
+          </n-select>
         </n-space>
       </n-descriptions-item>
     </n-descriptions>
@@ -26,6 +30,7 @@
 </template>
 
 <script setup lang="ts">
+  import { AccessTimeFilled } from '@vicons/material'
   import {ref,Ref } from "vue";
 
   const time = ref()
@@ -33,7 +38,28 @@
   const endTime = ref()
   const arbitrarilyTime = ref(1803023)
   const hours:Ref<number[]> = ref([])
-
+  const option = ref([
+    {
+      label:'08:00',
+      value:"08:00",
+    },
+    {
+      label:'08:30',
+      value:'08:30',
+    },
+    {
+      label:'09:00',
+      value:"09:00"
+    },
+    {
+      label:'09:30',
+      value:"09:30"
+    },
+    {
+      label:"10:00",
+      value:"10:00"
+    }
+  ])
   for (let i = 8; i < 18; i++) {
       hours.value.push(i);
   }
