0ece29c91e4501471f55a2781e807e6621a3c3d5
mr.wu <wushijiang@huan.tv>
2023-08-31 10:55:36 +0800

    test: 测试自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/vite-naive-admin/',
   resolve:{
     alias: {
         "@": path.resolve(__dirname, "src/"),
