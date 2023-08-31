6813111f8e3d3babcf54daa3914a67c9cf749e42
mr.wu <wushijiang@huan.tv>
2023-08-31 13:48:11 +0800

    test: 测试自动部署

--- src/config/global/permissions.ts
+++
@@ -1,7 +0,0 @@
-import { app }  from "@/config/vue/app";
-import { useStore } from "@pinia";
-app.config.globalProperties.$permissions = (code: string[] | string) => {
-   const store = useStore();
-   return store.getPower(code);
-};
-
--- src/views/other/multiRouter.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <n-alert  type="success" :show-icon="false">
-    <n-space vertical>
-      多级路由1-1-1-1
-      <n-input style="width:200px"/>
-    </n-space>
-  </n-alert>
-</template>
