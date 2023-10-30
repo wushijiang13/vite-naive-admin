af039ca5b953c47c6ab5784c6a4041cf4d8115e7
mr.wu <wushijiang@huan.tv>
2023-10-30 15:00:37 +0800

    test: 测试github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'//',
+  base:'./',
   resolve:{
     alias: {
         "@": path.resolve(__dirname, "src/"),
@@ -18,7 +18,8 @@ export default defineConfig({
     extensions: ['.js', '.json', '.ts']
   },
   build:{
-      outDir:"docs"
+      outDir:"docs",
+      assetsDir:'static'
   },
   plugins: [
       vue(),
